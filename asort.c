826941deef7c4b3fe0bc40632c72b27c6d181ea4
Geir Hauge <geir.hauge@gmail.com>
2016-01-27 14:44:18 +0100

    Handle multiple arrays

--- asort.c
+++ asort.c
@@ -13,51 +13,57 @@ int
 asort_builtin(list)
     WORD_LIST *list;
 {
-
     SHELL_VAR *var;
     ARRAY *a;
     ARRAY_ELEMENT *ae, *n;
     ARRAY_ELEMENT **sa;
     size_t i;
     int done = 0;
+    char *word;
 
     if (list == 0) {
         builtin_usage();
         return(EX_USAGE);
     }
 
-    var = find_variable(list->word->word);
-    if (var == 0 || array_p(var) == 0) {
-        // FIXME: find the builtin equivalent for report_error
-        report_error("%s: Not an array", list->word->word);
-        return (EXECUTION_FAILURE);
+    while (list) {
+        word = list->word->word;
+        list = list->next;
+        var = find_variable(word);
+
+        if (var == 0 || array_p(var) == 0) {
+            // FIXME: find the builtin equivalent for report_error
+            report_error("%s: Not an array", word);
+            return (EXECUTION_FAILURE);
+        }
+
+        a = array_cell(var);
+
+        if (a->num_elements == 0)
+            continue;
+
+        sa = xmalloc(a->num_elements * sizeof(ARRAY_ELEMENT*));
+
+        i = 0;
+        for (ae = a->head->next; ae != a->head; ae = ae->next) {
+            sa[i++] = ae;
+        }
+
+        qsort(sa, a->num_elements, sizeof(ARRAY_ELEMENT*), string_compare);
+
+        sa[0]->prev = sa[a->num_elements-1]->next = a->head;
+        a->head->next = sa[0];
+        a->head->prev = sa[a->num_elements-1];
+        a->max_index = a->num_elements-1;
+        for (i = 0; i < a->num_elements; i++) {
+            sa[i]->ind = i;
+            if (i > 0)
+                sa[i]->prev = sa[i-1];
+            if (i < a->num_elements - 1)
+                sa[i]->next = sa[i+1];
+        }
+        free(sa);
     }
-
-    a = array_cell(var);
-
-    sa = xmalloc(a->num_elements * sizeof(ARRAY_ELEMENT*));
-
-    i = 0;
-    for (ae = a->head->next; ae != a->head; ae = ae->next) {
-        sa[i++] = ae;
-    }
-
-    qsort(sa, a->num_elements, sizeof(ARRAY_ELEMENT*), string_compare);
-
-    sa[0]->prev = sa[a->num_elements-1]->next = a->head;
-    a->head->next = sa[0];
-    a->head->prev = sa[a->num_elements-1];
-    a->max_index = a->num_elements-1;
-    for (i = 0; i < a->num_elements; i++) {
-        sa[i]->ind = i;
-        if (i > 0)
-            sa[i]->prev = sa[i-1];
-        if (i < a->num_elements - 1)
-            sa[i]->next = sa[i+1];
-    }
-
-    xfree(sa);
-
     return (EXECUTION_SUCCESS);
 }
 
